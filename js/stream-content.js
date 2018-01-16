2d12c7cb3a8d681b1eb00c1211c0b30b85251693
GG4zzaneo <gius3ppe95@gmail.com>
2018-01-16 22:07:31 +0000

    Js Uncomment calling statement

--- js/stream-content.js
+++ js/stream-content.js
@@ -4,7 +4,7 @@
 	let contentExample = [ "#0275d8", "#5cb85c", "#5bc0de", "#f0ad4e", "#d9534f" ];
 	let contentCount = 0;
 	
-	//setInterval(changeMainContent, 3000);
+	setInterval(changeMainContent, 3000);
 	
 	function changeMainContent() {
 		mainContent.style.backgroundColor = contentExample[contentCount];
