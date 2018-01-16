fb232a4dc9b0af7e6aba5a4fc6a14dd18c83cf8b
GG4zzaneo <gius3ppe95@gmail.com>
2018-01-16 22:22:33 +0000

    Js Debugging

--- js/stream-content.js
+++ js/stream-content.js
@@ -6,20 +6,17 @@
 	let contentExample = [ "#0275d8", "#5cb85c", "#5bc0de", "#f0ad4e", "#d9534f" ];
 	let contentCount = 0;
 	
-	alert("start");
-	
 	setInterval(changeMainContent, 3000);
 	
 	function changeMainContent() {
-		alert("Function called");
+		console.log(contentCount);
+		
 		mainContent.style.backgroundColor = contentExample[contentCount];
-		alert("Colour swap");
-		if (contentCount === contentExample.length - 1) {
+		if (contentCount < contentExample.length - 1) {
 			contentCount++;
 		} else {
 			contentCount = 0;
 		}
-		
 	}
 	
 })()
