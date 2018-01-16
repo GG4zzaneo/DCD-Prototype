be0b72b0796d3caba2f04793f02c8dbcb5720356
GG4zzaneo <gius3ppe95@gmail.com>
2018-01-16 22:12:11 +0000

    Debugging Mobile

--- js/stream-content.js
+++ js/stream-content.js
@@ -1,14 +1,25 @@
+"use strict";
+
 (function(){
 	
 	let mainContent = document.getElementById("main-content");
 	let contentExample = [ "#0275d8", "#5cb85c", "#5bc0de", "#f0ad4e", "#d9534f" ];
 	let contentCount = 0;
 	
+	alert("start");
+	
 	setInterval(changeMainContent, 3000);
 	
 	function changeMainContent() {
+		alert("Function called");
 		mainContent.style.backgroundColor = contentExample[contentCount];
-		contentCount++;
+		alert("Colour swap");
+		if (contentCount === contentExample.length - 1) {
+			contentCount++;
+		} else {
+			contentCount = 0;
+		}
+		
 	}
 	
 })()
